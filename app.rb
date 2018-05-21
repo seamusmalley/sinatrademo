ccb3771da156973747d6e38ea9e5ccdb3c80504d
seamusmalley <seamalley@gmail.com>
2018-05-20 22:26:57 -0400

    /json has list as json, no indexing

--- app.rb
+++ app.rb
@@ -1,7 +1,12 @@
 require 'sinatra'
+require 'sinatra/json'
 
 get '/' do
     @list  = File.open('cake.list', 'r')
     @count = 1
     erb :print_list
+end
+
+get '/json' do
+    json File.read('cake.list')
 end
